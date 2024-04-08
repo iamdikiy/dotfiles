f88be1c5b4f96a356dfefac89c0ad1daf8296ee9
dikiy d <dimitridroll@gmail.com>
2024-04-08 14:20:51 +0300

    added some new plugin to my nvim <3

--- .config/nvim/lua/dikiyd/core/keymaps.lua
+++ .config/nvim/lua/dikiyd/core/keymaps.lua
@@ -5,7 +5,6 @@ local keymap = vim.keymap -- for conciseness
 
 ---------------------
 -- General Keymaps -------------------
-
 -- use jk to exit insert mode
 keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
 
--- .config/nvim/lua/dikiyd/lazy.lua
+++ .config/nvim/lua/dikiyd/lazy.lua
@@ -11,7 +11,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("dikiyd.plugins", {
+require("lazy").setup({ { import = "dikiyd.plugins" }, { import = "dikiyd.plugins.lsp" } }, {
   checker = {
     enabled = true,
     notify = false,
